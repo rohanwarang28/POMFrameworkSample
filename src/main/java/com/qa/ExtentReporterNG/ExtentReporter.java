92900d34af815dfc2ae4b0b50ac619c91a5d366b
rohan <rohanwarang28@yahoo.com>
2021-09-19 18:58:23 +0530

    commit 4

--- src/main/java/com/qa/ExtentReporterNG/ExtentReporter.java
+++ src/main/java/com/qa/ExtentReporterNG/ExtentReporter.java
@@ -14,58 +14,25 @@ import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.xml.XmlSuite;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-public class ExtentReporter implements IReporter {
-	public ExtentReports extent;
-
-	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-			String outputDirectory) {
-		extent = new ExtentReports(outputDirectory + File.separator
-				+ "FreeCRM_Extent.html", true);
-
-		for (ISuite suite : suites) {
-			Map<String, ISuiteResult> result = suite.getResults();
-
-			for (ISuiteResult r : result.values()) {
-				ITestContext context = r.getTestContext();
-
-				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
-				buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
-				buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
-			}
+public class ExtentReporter  {
+	
+	static ExtentReports extent ; 
+	
+		public static ExtentReports getReportObject() {
+			
+			String path = System.getProperty("user.dir")+"\\reports\\index.html";
+			ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+			reporter.config().setReportName("Web Automation results");
+			reporter.config().setDocumentTitle("Test Results");
+			extent = new ExtentReports();
+			extent.attachReporter(reporter);
+			extent.setSystemInfo("tester", "Rohan W");
+			
+			return extent;
 		}
-
-		extent.flush();
-		extent.close();
-	}
-
-	private void buildTestNodes(IResultMap tests, LogStatus status) {
-		ExtentTest test;
-
-		if (tests.size() > 0) {
-			for (ITestResult result : tests.getAllResults()) {
-				test = extent.startTest(result.getMethod().getMethodName());
-
-				test.setStartedTime(getTime(result.getStartMillis()));
-				test.setEndedTime(getTime(result.getEndMillis()));
-
-				for (String group : result.getMethod().getGroups())
-					test.assignCategory(group);
-
-				if (result.getThrowable() != null) {
-					test.log(status, result.getThrowable());
-				} else {
-					test.log(status, "Test " + status.toString().toLowerCase()
-							+ "ed");
-				}
-
-				extent.endTest(test);
-			}
-		}
-	}
 
 	private Date getTime(long millis) {
 		Calendar calendar = Calendar.getInstance();
